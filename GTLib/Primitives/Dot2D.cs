10dfffe5b233ab3d164c780d37821bfda4a5eb38
Xilosof <pro100temi444@gmail.com>
2020-05-23 20:47:12 +0300

    Create 2D Dot class

---
+++ GTLib/Primitives/Dot2D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTLib.Interfaces;
+
+namespace GTLib.Primitives
+{
+    class Dot2D:IGTDrawable
+    {
+        int X { get; set; }
+        int Y { get; set; }
+        public bool Draw()
+        {
+            //TODO
+            return false;
+        }
+    }
+}
