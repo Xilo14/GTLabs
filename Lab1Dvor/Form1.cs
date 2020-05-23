1b1e61de669f1389a40ea7652d8ceda5ae52ae28
Xilosof <pro100temi444@gmail.com>
2020-05-23 17:59:19 +0300

    Change structure. Now Lab1 is .NET Core WinForms. Deleted VBitmap

--- Lab1Dvor/Form1.cs
+++ Lab1Dvor/Form1.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab1Dbornik
+namespace Lab1Dvor
 {
     public partial class Form1 : Form
     {
@@ -18,10 +17,5 @@ namespace Lab1Dbornik
             InitializeComponent();
         }
 
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            Debug.WriteLine(e.KeyChar);            
-            
-        }
     }
 }
