d7d752b359a012cf4218cd8d6499f8a0615d96a1
Xilosof <pro100temi444@gmail.com>
2020-05-24 00:46:50 +0300

    Created abstract GTDrawer. His inheritos takes scene2D and visualize his on a Bitmap

---
+++ GTLib/Drawers/GTDrawer.cs
@@ -0,0 +1,26 @@
+﻿using GTLib.Interfaces;
+using GTLib.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace GTLib.Drawers
+{
+    public class GTDrawer : IGTDrawing
+    {
+        public IGTHavingPrimitives2D scene { get; set; }
+        public Bitmap bitmap { get; set; }
+
+        public GTDrawer(IGTHavingPrimitives2D scene,Bitmap bitmap)
+        {
+            this.scene = scene;
+            this.bitmap = bitmap;
+        }
+        public GTDrawer() : this(new Scene2D(),new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
+        public GTDrawer(IGTHavingPrimitives2D scene) : this(scene, new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
+        public GTDrawer(Bitmap bitmap) : this(new Scene2D(), bitmap) { }
+
+
+    }
+}
---
+++ GTLib/Drawers/GTDrawerSlow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTLib.Interfaces;
+using GTLib.Scenes;
+
+namespace GTLib.Drawers
+{
+    class GTDrawerSlow:GTDrawer
+    {
+        GTDrawerSlow(ref Scene2D scene)
+        {
+
+        }
+    }
+}
