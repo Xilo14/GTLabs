3f1dbbec28e4dd9cebe767c01d8f3363d6a3bf14
Xilosof <pro100temi444@gmail.com>
2020-05-23 21:16:04 +0300

    Circle class with Dot2D center and radius

---
+++ GTLib/Primitives/Circle2D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Primitives
+{
+    class Circle2D
+    {
+        Dot2D Center { get; set; }
+        int Radius { get; set; }
+    }
+}
