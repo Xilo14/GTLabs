62f76b8d92a397f068a7c165e7806221886b9c4e
Xilosof <pro100temi444@gmail.com>
2020-05-24 00:38:19 +0300

    Create static class to save all GLOBAL vars

---
+++ GTLib/EnvVar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib
+{
+    public static class EnvVar
+    {
+        public static int STANDART_BMP_HEIGHT = 800;
+        public static int STANDART_BMP_WIDTH = 800;
+    }
+}
