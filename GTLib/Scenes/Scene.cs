51faa8fa143d594ef9114a548125301c9671decc
Xilosof <pro100temi444@gmail.com>
2020-05-24 00:36:46 +0300

    Init abstract class Scene and his inheritor Scene2D, which have 2d primitives or elements

---
+++ GTLib/Scenes/Scene.cs
@@ -0,0 +1,14 @@
+﻿using GTLib.Interfaces;
+using GTLib.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Scenes
+{
+    public abstract class Scene
+    {
+       
+
+    }
+}
---
+++ GTLib/Scenes/Scene2D.cs
@@ -0,0 +1,22 @@
+﻿using GTLib.Interfaces;
+using GTLib.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Scenes
+{
+    public class Scene2D : Scene,IGTHavingPrimitives2D
+    {
+        public List<Primitive2D> _elements;
+
+        public List<Primitive2D> GetElements()
+        {
+            return _elements;
+        }
+        public Scene2D()
+        {
+            _elements = new List<Primitive2D>();
+        }
+    }
+}
