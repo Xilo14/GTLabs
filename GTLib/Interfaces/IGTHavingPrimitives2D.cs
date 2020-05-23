e71cd2b4f61566a7faac10c329620f7c00eef156
Xilosof <pro100temi444@gmail.com>
2020-05-24 00:50:32 +0300

    Created interface for classes hich contains 2d primitives. For example it is Scene2D

---
+++ GTLib/Interfaces/IGTHavingPrimitives2D.cs
@@ -0,0 +1,12 @@
+﻿using GTLib.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Interfaces
+{
+    public interface IGTHavingPrimitives2D
+    {
+        public List<Primitive2D> GetElements();
+    }
+}
