18abbf571c1cac73aa4877b4ae613a1573395d04
Xilosof <pro100temi444@gmail.com>
2020-05-24 00:49:24 +0300

    Refactor. Now Element2D its inheritor Primitive2D. Class Element no longer exists

---
+++ GTLib/Elements/Element2D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTLib.Primitives;
+
+namespace GTLib.Elements
+{
+    abstract class Element2D:Primitive2D
+    {
+    }
+}
