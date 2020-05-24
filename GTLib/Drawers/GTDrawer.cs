1c817e39fb295f4c93f882a111e60af6eee2b556
Xilosof <pro100temi444@gmail.com>
2020-05-24 18:15:00 +0300

    Refactor GTDrawer. Now he is absract

--- GTLib/Drawers/GTDrawer.cs
+++ GTLib/Drawers/GTDrawer.cs
@@ -4,23 +4,45 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Diagnostics;
+using GTLib.Primitives;
+using GTLib.Elements;
 
 namespace GTLib.Drawers
 {
-    public class GTDrawer : IGTDrawing
+    public abstract class GTDrawer : IGTDrawing
     {
         public IGTHavingPrimitives2D scene { get; set; }
         public Bitmap bitmap { get; set; }
 
-        public GTDrawer(IGTHavingPrimitives2D scene,Bitmap bitmap)
+        public GTDrawer(IGTHavingPrimitives2D scene, Bitmap bitmap)
         {
             this.scene = scene;
             this.bitmap = bitmap;
         }
-        public GTDrawer() : this(new Scene2D(),new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
+        public GTDrawer() : this(new Scene2D(), new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
         public GTDrawer(IGTHavingPrimitives2D scene) : this(scene, new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
         public GTDrawer(Bitmap bitmap) : this(new Scene2D(), bitmap) { }
 
+        public void Draw()
+        {
+            
+        }
+
+        public UInt32 DrawWithMetric()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            this.Draw();
+
+            stopWatch.Stop();
+            return (UInt32)stopWatch.ElapsedMilliseconds;
+        }
+
+        
+        
 
     }
+    
 }
