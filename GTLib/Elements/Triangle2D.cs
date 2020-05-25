96378b7eda806b7cdb1109d5bdb55c9b01a88415
Xilosof <pro100temi444@gmail.com>
2020-05-25 15:58:15 +0300

    Now, if u replace one of the dots list Primitives will be updated to save correct links on the dots

--- GTLib/Elements/Triangle2D.cs
+++ GTLib/Elements/Triangle2D.cs
@@ -1,22 +1,61 @@
 ﻿using GTLib.Primitives;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace GTLib.Elements
 {
-    class Triangle2D:Element2D
+    public class Triangle2D:Element2D
     {
-        public Dot2D a { get; set; }
-        public Dot2D b { get; set; }
-        public Dot2D c { get; set; }
+        private Dot2D _a;
+
+        public Dot2D a
+        {
+            get => _a;
+            set
+            {
+                _a = value;
+                ((Line2D)this.Primitives[0]).start = this._a;
+                ((Line2D)this.Primitives[2]).finish = this._a;
+            }
+
+        }
+
+        private Dot2D _b;
+        public Dot2D b
+        {
+            get => _b;
+            set
+            {
+                _b = value;
+                ((Line2D)this.Primitives[0]).finish = this._b;
+                ((Line2D)this.Primitives[1]).start = this._b;
+            }
+
+        }
+
+        private Dot2D _c;
+        public Dot2D c
+        {
+            get => _c;
+            set
+            {
+                _c = value;
+                ((Line2D)this.Primitives[1]).finish = this._c;
+                ((Line2D)this.Primitives[2]).start = this._c;
+            }
+
+        }
 
         public Triangle2D(Dot2D a,Dot2D b,Dot2D c)
         {
-            this.Primitives = new List<Primitive2D>();
-            this.Primitives.Add(new Line2D(a,b));
-            this.Primitives.Add(new Line2D(b,c));
-            this.Primitives.Add(new Line2D(c,a));
+            this.Primitives = new List<Primitive2D>
+            {
+                new Line2D(a, b),
+                new Line2D(b, c),
+                new Line2D(c, a)
+            };
             this.a = a;
             this.b = b;
             this.c = c;
