6e8ef3da1ac8a4dbe7cf52a28ffeb59aca196dea
Xilosof <pro100temi444@gmail.com>
2020-05-27 18:44:47 +0300

    Delete bitmap from parenat class,because he is slowly. We will try find better solution

--- GTLib/Drawers/GTDrawer.cs
+++ GTLib/Drawers/GTDrawer.cs
@@ -1,51 +1,28 @@
-﻿using GTLib.Interfaces;
-using GTLib.Scenes;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Drawing;
-using System.Diagnostics;
-using GTLib.Primitives;
-using GTLib.Elements;
+﻿using System.Diagnostics;
+using GTLib.Interfaces;
 
 namespace GTLib.Drawers
 {
     public abstract class GTDrawer : IGTDrawing
     {
         public IGTHavingPrimitives2D scene { get; set; }
-        public Bitmap bitmap { get; set; }
-
-        public GTDrawer(IGTHavingPrimitives2D scene, Bitmap bitmap)
-        {
-            this.scene = scene;
-            this.bitmap = bitmap;
-        }
-        public GTDrawer() : this(new Scene2D(), new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
-        public GTDrawer(IGTHavingPrimitives2D scene) : this(scene, new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
-        public GTDrawer(Bitmap bitmap) : this(new Scene2D(), bitmap) { }
 
         public virtual void Draw()
         {
-            
         }
 
-        public virtual UInt32 DrawWithMetric()
+        public virtual uint DrawWithMetric()
         {
-            Stopwatch stopWatch = new Stopwatch();
+            var stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            this.Draw();
+            Draw();
 
             stopWatch.Stop();
             //return (UInt32)stopWatch.ElapsedTicks;
-            double seconds = (double)stopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+            var seconds = stopWatch.ElapsedTicks / (double) Stopwatch.Frequency;
             var nanoseconds = seconds * 1000000000;
-            return (UInt32)nanoseconds;
+            return (uint) nanoseconds;
         }
-
-
-
-
     }
-    
 }
