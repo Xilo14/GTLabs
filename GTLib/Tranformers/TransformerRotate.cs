20215a24c8e0a01ec2c477727cadde1069817d38
Xilosof <pro100temi444@gmail.com>
2020-05-27 19:39:26 +0300

    Added method for scene

--- GTLib/Tranformers/TransformerRotate.cs
+++ GTLib/Tranformers/TransformerRotate.cs
@@ -3,40 +3,24 @@ using System.Collections.Generic;
 using GTLib.Elements;
 using GTLib.Interfaces;
 using GTLib.Primitives;
+using GTLib.Scenes;
 
 namespace GTLib.Tranformers
 {
     public class TransformerRotate : IGTTransformer2D
     {
-        private void RotateDot2D(Dot2D dot2d)
-        {
-            //Cord
-            var X = dot2d.X - Center.X;
-            var Y = dot2d.Y - Center.Y;
-
-            //Rotate
-            X = ( X * Math.Cos(this.Degree* Math.PI / 180)
-                                - Y * Math.Sin(this.Degree* Math.PI / 180));
-            Y = (X * Math.Sin(this.Degree* Math.PI / 180)
-                                + Y * Math.Cos(this.Degree* Math.PI / 180));
-
-            //Cord
-            dot2d.X = X + Center.X;
-            dot2d.Y = Y + Center.Y;
-        }
-
         private readonly Dictionary<Type, TransformMethod> _stdDictPrimitives =
             new Dictionary<Type, TransformMethod>
             {
                 {
-                    typeof(Dot2D), (self,primitive) =>
+                    typeof(Dot2D), (self, primitive) =>
                     {
                         var dot2d = (Dot2D) primitive;
                         self.RotateDot2D(dot2d);
                     }
                 },
                 {
-                    typeof(Line2D), (self,primitive )=>
+                    typeof(Line2D), (self, primitive) =>
                     {
                         var line2d = (Line2D) primitive;
                         self.RotateDot2D(line2d.start);
@@ -44,7 +28,7 @@ namespace GTLib.Tranformers
                     }
                 },
                 {
-                    typeof(Circle2D),(self,primitive) =>
+                    typeof(Circle2D), (self, primitive) =>
                     {
                         var circle2d = (Circle2D) primitive;
                         self.RotateDot2D(circle2d.Center);
@@ -52,23 +36,51 @@ namespace GTLib.Tranformers
                 }
             };
 
-        public double Degree { get; set; } = 0;
-        public Dot2D Center { get; set; }
-
         public TransformerRotate(Dot2D center)
         {
-            this.Center = center;
+            Center = center;
         }
-        public TransformerRotate() 
-            : this(new Dot2D(0, 0)) { }
+
+        public TransformerRotate()
+            : this(new Dot2D(0, 0))
+        {
+        }
+
+        public double Degree { get; set; } = 0;
+        public Dot2D Center { get; set; }
 
         public void Transform(Primitive2D primitive)
         {
             if (primitive is Element2D element2D)
-                foreach (var el in element2D.Primitives)
-                    this.Transform(el);
+                foreach (var el in element2D.DeclarativePrimitives)
+                    Transform(el);
             else
                 _stdDictPrimitives[primitive.GetType()](this, primitive);
+        }
+        public void Transform(Scene2D scene)
+        {
+            foreach (var el in scene.GetElements())
+                Transform(el);
+        }
+
+        private void RotateDot2D(Dot2D dot2d)
+        {
+            //Translate cord to local system
+            var X = dot2d.X - Center.X;
+            var Y = dot2d.Y - Center.Y;
+
+            //Rad
+            var rad = Degree * (Math.PI / 180);
+
+            //Rotate
+            var newX = X * Math.Cos(rad)
+                       - Y * Math.Sin(rad);
+            var newY = X * Math.Sin(rad)
+                       + Y * Math.Cos(rad);
+
+            //Translate cord back to global system
+            dot2d.X = newX + Center.X;
+            dot2d.Y = newY + Center.Y;
         }
 
         private delegate void TransformMethod(TransformerRotate self, Primitive2D primitive);
