08b85e428de458cd25bc1d0562ba7c168f2be028
Юрий Дьяченко <yurokke.yury@gmail.com>
2020-05-28 02:38:42 +0300

    fixed algorithm for parser

--- GTLib/FileParsers/WaveFrontParser.cs
+++ GTLib/FileParsers/WaveFrontParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Text;
 using GTLib.Elements;
@@ -24,50 +25,143 @@ namespace GTLib
                 if (str.StartsWith("v "))
                 {
                     string[] words = str.Split(new char[] { '/', ' ' });
-
+                    string[] swords = new string[10];
+                    int i = 0; int j = 0;
+                    while (i < 3)
+                    {
+                        if (!String.IsNullOrEmpty(words[j]))
+                        {
+                            swords[i] = words[j];
+                            i++;
+                        }
+                        j++;
+                    }
                     model.v.Add(new Dot3D(
-                        Convert.ToDouble(words[1], provider),
-                        Convert.ToDouble(words[2], provider),
-                        Convert.ToDouble(words[3], provider)));
+                        Convert.ToDouble(swords[1], provider),
+                        Convert.ToDouble(swords[2], provider),
+                        Convert.ToDouble(swords[3], provider)));
                 }
                 else if (str.StartsWith("vn "))
                 {
                     string[] words = str.Split(new char[] { '/', ' ' });
-
+                    string[] swords = new string[10];
+                    int i = 0; int j = 0;
+                    while (i < 3)
+                    {
+                        if (!String.IsNullOrEmpty(words[j]))
+                        {
+                            swords[i] = words[j];
+                            i++;
+                        }
+                        j++;
+                    }
                     model.vn.Add(new Vector3(
-                        Convert.ToSingle(words[2], provider),
-                        Convert.ToSingle(words[3], provider),
-                        Convert.ToSingle(words[4], provider)));
+                        Convert.ToSingle(swords[1], provider),
+                        Convert.ToSingle(swords[2], provider),
+                        Convert.ToSingle(swords[3], provider)));
                 }
                 else if (str.StartsWith("vt "))
                 {
                     string[] words = str.Split(new char[] { '/', ' ' });
-
+                    string[] swords = new string[10];
+                    int i = 0; int j = 0;
+                    while (i < 2)
+                    {
+                        if (!String.IsNullOrEmpty(words[j]))
+                        {
+                            swords[i] = words[j];
+                            i++;
+                        }
+                        j++;
+                    }
                     model.vt.Add(new Vector2(
-                        Convert.ToSingle(words[2], provider),
-                        Convert.ToSingle(words[3], provider)));
+                        Convert.ToSingle(swords[1], provider),
+                        Convert.ToSingle(swords[2], provider)));
                 }
                 else if (str.StartsWith("f "))
                 {
                     List<Vector3> f = new List<Vector3>();
+
+
+                    //string[] words = str.TrimStart('f').Trim(' ').Split(new char[]{'/', ' '});
                     
+                    string[] words = str.Split(new char[] { '/', ' ' });
+                    if(words.Length > 10) {
+                        string[] swords = new string[10];
+                        int i = 0; int j = 0;
+                        while (i < 9)
+                        {
+                            if (!String.IsNullOrEmpty(words[j]))
+                            {
+                                swords[i] = words[j];
+                                i++;
+                            }
+                            j++;
+                        }
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[1], provider),
+                            Convert.ToSingle(swords[2], provider),
+                            Convert.ToSingle(swords[3], provider)));
 
-                    string[] words = str.TrimStart('f').Split(new char[]{'/', ' '});
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[4], provider),
+                            Convert.ToSingle(swords[5], provider),
+                            Convert.ToSingle(swords[6], provider)));
 
-                    f.Add(new Vector3(
-                        Convert.ToSingle(words[1], provider),
-                        Convert.ToSingle(words[2], provider),
-                        Convert.ToSingle(words[3], provider)));
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[7], provider),
+                            Convert.ToSingle(swords[8], provider),
+                            Convert.ToSingle(swords[9], provider)));
+                    }
+                    if (words.Length < 10 && words.Length > 6)
+                    {
+                        string[] swords = new string[10];
+                        int i = 0; int j = 0;
+                        while (i < 6)
+                        {
+                            if (!String.IsNullOrEmpty(words[j]))
+                            {
+                                swords[i] = words[j];
+                                i++;
+                            }
+                            j++;
+                        }
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[1], provider),
+                            Convert.ToSingle(swords[2], provider),
+                            Convert.ToSingle(swords[3], provider)));
 
-                    f.Add(new Vector3(
-                        Convert.ToSingle(words[4], provider),
-                        Convert.ToSingle(words[5], provider),
-                        Convert.ToSingle(words[6], provider)));
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[4], provider),
+                            Convert.ToSingle(swords[5], provider),
+                            Convert.ToSingle(swords[6], provider)));
 
-                    f.Add(new Vector3(
-                        Convert.ToSingle(words[7], provider),
-                        Convert.ToSingle(words[8], provider),
-                        Convert.ToSingle(words[9], provider)));
+                        f.Add(new Vector3(1, 1, 1));
+                    }
+                    if (words.Length < 6 )
+                    {
+                        string[] swords = new string[10];
+                        int i = 0; int j = 0;
+                        while (i < 3)
+                        {
+                            if (!String.IsNullOrEmpty(words[j]))
+                            {
+                                swords[i] = words[j];
+                                i++;
+                            }
+                            j++;
+                        }
+                        f.Add(new Vector3(
+                            Convert.ToSingle(swords[1], provider),
+                            Convert.ToSingle(swords[2], provider),
+                            Convert.ToSingle(swords[3], provider)));
+
+                        f.Add(new Vector3(1, 1, 1));
+
+                        f.Add(new Vector3(1, 1, 1));
+                    }
+
+
 
                     model.f.Add(f);
                 }
