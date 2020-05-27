5a59dedf42e715c7892f0f5fa7dd86f5790d4a46
Xilosof <pro100temi444@gmail.com>
2020-05-27 18:55:45 +0300

    Added to all elements list DeclarativePrimitives. As result, we have two lists DeclarativePrimitives and Primitives. In Primitives stored drawable primitives, Drawer take their and visualize. In DeclarativePrimitives stored primitives which determine some element. For example triangle is defined by three points, but drawn using three lines.

--- GTLib/Elements/Element2D.cs
+++ GTLib/Elements/Element2D.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using GTLib.Primitives;
 
 namespace GTLib.Elements
 {
-    public abstract class Element2D:Primitive2D
+    public abstract class Element2D : Primitive2D
     {
         public List<Primitive2D> Primitives { get; protected set; }
+        public List<Primitive2D> DeclarativePrimitives { get; protected set; }
     }
 }
