153239cdfc2096f7c7c6a29bbcf035720b5bb7be
Xilosof <pro100temi444@gmail.com>
2020-05-27 18:47:45 +0300

    Refactor class DrowerSlow. Now he translate bitmap to byte[,,] and change him. After translate back. Its more quickly than manipulate bitmap

--- GTLib/Drawers/GTDrawerSlow.cs
+++ GTLib/Drawers/GTDrawerSlow.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Drawing;
-using System.Text;
+using System.Drawing.Imaging;
 using GTLib.Elements;
 using GTLib.Interfaces;
 using GTLib.Primitives;
@@ -10,104 +9,32 @@ using GTLib.Scenes;
 
 namespace GTLib.Drawers
 {
-    public class GTDrawerSlow : GTDrawer
+    public unsafe class GTDrawerSlow : GTDrawer
     {
         public enum AlgsForLine
         {
             Bresenham,
-            Luke,
+            Luke
         }
-
-        public GTDrawerSlow(IGTHavingPrimitives2D scene, Bitmap bitmap)
-            :base( scene, bitmap){}
-        public GTDrawerSlow() : this(new Scene2D(),
-            new Bitmap(EnvVar.STANDART_BMP_WIDTH,
-                EnvVar.STANDART_BMP_HEIGHT)) { }
-        public GTDrawerSlow(IGTHavingPrimitives2D scene) : this(scene, new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT)) { }
-        public GTDrawerSlow(Bitmap bitmap) : this(new Scene2D(), bitmap) { }
-
-        public AlgsForLine CurrentAlgForLine { get; set; } = AlgsForLine.Bresenham;
-
-        public override void Draw()
-        {
-            foreach (var element in scene.GetElements())
-                DrawElement2D(element);
-        }
-
-        private void DrawPrimitive2D(Primitive2D primitive)
-        {
-            _stdDictPrimitives[primitive.GetType()](this, primitive);
-        }
-        private void DrawElement2D(Primitive2D element)
-        {
-            if (element is Element2D element2D)
-                foreach (var el in element2D.Primitives)
-                    DrawElement2D(el);
-            else
-                DrawPrimitive2D(element);
-        }
-
-       
-
-        private readonly Dictionary<Type, DrawMethod> _stdDictPrimitives = 
-            new Dictionary<Type, DrawMethod>
-        {
-            { typeof(Dot2D), (GTDrawerSlow self,Primitive2D primitive) => {
-                Dot2D dot2d = (Dot2D)primitive;
-                self.bitmap.SetPixel((int)dot2d.X,(int)dot2d.Y,EnvVar.STD_COLOR);
-            } },
-            { typeof(Line2D), (GTDrawerSlow self,Primitive2D primitive) => {
-                Line2D line2d = (Line2D)primitive;
-                self._algsDrawingLines[self.CurrentAlgForLine](self,line2d);
-            } },
-            { typeof(Circle2D), (GTDrawerSlow self,Primitive2D primitive) => {
-                Circle2D circle2d = (Circle2D)primitive;
-                int Xc = (int)circle2d.Center.X,
-                    Yc = (int)circle2d.Center.Y;
-                int x = 0;
-                int y = (int)circle2d.Radius;
-                int delta = 1 - 2 * (int)circle2d.Radius;
-                int error = 0;
-
-                while (y >= 0)
-                {
-                    self.bitmap.SetPixel(Xc+x, Yc+y, EnvVar.STD_COLOR);
-                    self.bitmap.SetPixel(Xc + x, Yc - y, EnvVar.STD_COLOR);
-                    self.bitmap.SetPixel(Xc - x, Yc + y, EnvVar.STD_COLOR);
-                    self.bitmap.SetPixel(Xc - x, Yc - y, EnvVar.STD_COLOR);
-                    error = 2 * (delta + y) - 1;
-                    if ((delta < 0) && (error <= 0))
-                    {
-                        delta += 2 * (x++) + 1;
-                        continue;
-                    }
-                    if ((delta > 0) && (error > 0))
-                    {
-                        delta -= 2 * (y--) + 1;
-                        continue;
-                    }
-                    delta += 2 * (x++ - y--);
-                }
-            } },
-        };
 
         private readonly Dictionary<AlgsForLine, AlgorithmDrawingLine> _algsDrawingLines =
             new Dictionary<AlgsForLine, AlgorithmDrawingLine>
             {
                 {
-                    AlgsForLine.Luke, (GTDrawerSlow self, Line2D line) =>
+                    AlgsForLine.Luke, (self, line) =>
                     {
-                        int Xd = (int)line.start.X;
-                        int Yd = (int)line.start.Y;
-                        int Xf = (int)line.finish.X;
-                        int Yf = (int)line.finish.Y;
+                        var Xd = (int) line.start.X;
+                        var Yd = (int) line.start.Y;
+                        var Xf = (int) line.finish.X;
+                        var Yf = (int) line.finish.Y;
 
                         int Dx, Dy, Cumul;
                         int Xinc, Yinc, X, Y;
                         int i;
                         X = Xd;
                         Y = Yd;
-                        self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                        //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                        self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
                         if (Xd < Xf)
                             Xinc = 1;
                         else
@@ -132,7 +59,8 @@ namespace GTLib.Drawers
                                 }
 
                                 //Sleep(10);
-                                self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
                             }
                         }
                         else
@@ -149,18 +77,19 @@ namespace GTLib.Drawers
                                 }
 
                                 //Sleep(10);
-                                self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
                             }
                         }
                     }
                 },
                 {
-                    AlgsForLine.Bresenham, (GTDrawerSlow self, Line2D line) =>
+                    AlgsForLine.Bresenham, (self, line) =>
                     {
-                        int Xd = (int)line.start.X;
-                        int Yd = (int)line.start.Y;
-                        int Xf = (int)line.finish.X;
-                        int Yf = (int)line.finish.Y;
+                        var Xd = (int) line.start.X;
+                        var Yd = (int) line.start.Y;
+                        var Xf = (int) line.finish.X;
+                        var Yf = (int) line.finish.Y;
 
                         int Dx, Dy, Dx2, Dy2, Dxy, S, Xinc, Yinc, X, Y, i;
 
@@ -179,7 +108,8 @@ namespace GTLib.Drawers
                         Dy2 = Dy + Dy;
                         X = Xd;
                         Y = Yd;
-                        self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                        //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                        self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
 
                         if (Dx > Dy)
                         {
@@ -193,10 +123,13 @@ namespace GTLib.Drawers
                                     S = S + Dxy;
                                 }
                                 else
+                                {
                                     S = S + Dy2;
+                                }
 
                                 X = X + Xinc;
-                                self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
                             }
                         }
                         else
@@ -211,16 +144,246 @@ namespace GTLib.Drawers
                                     S = S + Dxy;
                                 }
                                 else
+                                {
                                     S = S + Dx2;
+                                }
 
                                 Y = Y + Yinc;
-                                self.bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                //self.Bitmap.SetPixel(X, Y, EnvVar.STD_COLOR);
+                                self._setPixelInBytes(X, Y, EnvVar.STD_COLOR);
                             }
                         }
                     }
-                },
+                }
             };
+
+
+        private readonly Dictionary<Type, DrawMethod> _stdDictPrimitives =
+            new Dictionary<Type, DrawMethod>
+            {
+                {
+                    typeof(Dot2D), (self, primitive) =>
+                    {
+                        var dot2d = (Dot2D) primitive;
+                        //self.Bitmap.SetPixel((int)dot2d.X,(int)dot2d.Y,EnvVar.STD_COLOR);
+                        self._setPixelInBytes((int) dot2d.X, (int) dot2d.Y, EnvVar.STD_COLOR);
+                    }
+                },
+                {
+                    typeof(Line2D), (self, primitive) =>
+                    {
+                        var line2d = (Line2D) primitive;
+                        self._algsDrawingLines[self.CurrentAlgForLine](self, line2d);
+                    }
+                },
+                {
+                    typeof(Circle2D), (self, primitive) =>
+                    {
+                        var circle2d = (Circle2D) primitive;
+                        int Xc = (int) circle2d.Center.X,
+                            Yc = (int) circle2d.Center.Y;
+                        var x = 0;
+                        var y = (int) circle2d.Radius;
+                        var delta = 1 - 2 * (int) circle2d.Radius;
+                        var error = 0;
+
+                        while (y >= 0)
+                        {
+                            //self.Bitmap.SetPixel(Xc+x, Yc+y, EnvVar.STD_COLOR);
+                            //self.Bitmap.SetPixel(Xc + x, Yc - y, EnvVar.STD_COLOR);
+                            //self.Bitmap.SetPixel(Xc - x, Yc + y, EnvVar.STD_COLOR);
+                            //self.Bitmap.SetPixel(Xc - x, Yc - y, EnvVar.STD_COLOR);
+                            self._setPixelInBytes(Xc + x, Yc + y, EnvVar.STD_COLOR);
+                            self._setPixelInBytes(Xc + x, Yc - y, EnvVar.STD_COLOR);
+                            self._setPixelInBytes(Xc - x, Yc + y, EnvVar.STD_COLOR);
+                            self._setPixelInBytes(Xc - x, Yc - y, EnvVar.STD_COLOR);
+                            error = 2 * (delta + y) - 1;
+                            if (delta < 0 && error <= 0)
+                            {
+                                delta += 2 * x++ + 1;
+                                continue;
+                            }
+
+                            if (delta > 0 && error > 0)
+                            {
+                                delta -= 2 * y-- + 1;
+                                continue;
+                            }
+
+                            delta += 2 * (x++ - y--);
+                        }
+                    }
+                }
+            };
+
+        private Bitmap _bitmap;
+        private bool _bitmapReady;
+        private byte[,,] _bytes;
+
+        public GTDrawerSlow(IGTHavingPrimitives2D scene, Bitmap bitmap)
+        {
+            this.scene = scene;
+            Bitmap = bitmap;
+        }
+
+        public GTDrawerSlow() : this(new Scene2D(),
+            new Bitmap(EnvVar.STANDART_BMP_WIDTH,
+                EnvVar.STANDART_BMP_HEIGHT))
+        {
+        }
+
+        public GTDrawerSlow(IGTHavingPrimitives2D scene) : this(scene,
+            new Bitmap(EnvVar.STANDART_BMP_WIDTH, EnvVar.STANDART_BMP_HEIGHT))
+        {
+        }
+
+        public GTDrawerSlow(Bitmap bitmap) : this(new Scene2D(), bitmap)
+        {
+        }
+
+        public Bitmap Bitmap
+        {
+            get
+            {
+                if (!_bitmapReady)
+                    RgbToBitmapQ();
+                return _bitmap;
+            }
+            set
+            {
+                _bitmap = value;
+                BitmapToByteRgbQ();
+            }
+        }
+
+        public AlgsForLine CurrentAlgForLine { get; set; } = AlgsForLine.Bresenham;
+
+        public override void Draw()
+        {
+            //Array.Clear(_bytes, 0, _bytes.Length);
+            _bytes = new byte[3, _bitmap.Height, _bitmap.Width];
+            foreach (var element in scene.GetElements())
+                DrawElement2D(element);
+            _bitmapReady = false;
+        }
+
+        private void DrawPrimitive2D(Primitive2D primitive)
+        {
+            _stdDictPrimitives[primitive.GetType()](this, primitive);
+        }
+
+        private void DrawElement2D(Primitive2D element)
+        {
+            if (element is Element2D element2D)
+                foreach (var el in element2D.Primitives)
+                    DrawElement2D(el);
+            else
+                DrawPrimitive2D(element);
+        }
+
+        private void BitmapToByteRgbQ()
+        {
+            var bmp = _bitmap;
+            int width = bmp.Width,
+                height = bmp.Height;
+            var res = new byte[3, height, width];
+            var bd = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            try
+            {
+                fixed (byte* _res = res)
+                {
+                    byte* _r = _res, _g = _res + width * height, _b = _res + 2 * width * height;
+                    for (var h = 0; h < height; h++)
+                    {
+                        var curPos = (byte*) bd.Scan0 + h * bd.Stride;
+                        for (var w = 0; w < width; w++)
+                        {
+                            *_b = *curPos++;
+                            ++_b;
+                            *_g = *curPos++;
+                            ++_g;
+                            *_r = *curPos++;
+                            ++_r;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                bmp.UnlockBits(bd);
+                _bitmapReady = false;
+            }
+
+            _bytes = res;
+        }
+
+        private void RgbToBitmapQ()
+        {
+            var rgb = _bytes;
+            if (rgb.GetLength(0) != 3)
+                throw new ArrayTypeMismatchException(
+                    "Size of first dimension for passed array must be 3 (RGB components)");
+
+            int width = rgb.GetLength(2),
+                height = rgb.GetLength(1);
+
+            var result = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            var bd = result.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly,
+                PixelFormat.Format24bppRgb);
+
+            try
+            {
+                byte* curpos;
+                fixed (byte* _rgb = rgb)
+                {
+                    byte* _r = _rgb, _g = _rgb + width * height, _b = _rgb + 2 * width * height;
+                    for (var h = 0; h < height; h++)
+                    {
+                        curpos = (byte*) bd.Scan0 + h * bd.Stride;
+                        for (var w = 0; w < width; w++)
+                        {
+                            *curpos++ = *_b;
+                            ++_b;
+                            *curpos++ = *_g;
+                            ++_g;
+                            *curpos++ = *_r;
+                            ++_r;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                result.UnlockBits(bd);
+                _bitmapReady = true;
+            }
+
+            _bitmap = result;
+        }
+
+        private void _setPixelInBytes(int x, int y, Color color)
+        {
+            if (y >= _bitmap.Height || y < 0 ||
+                x >= _bitmap.Width || x < 0)
+                return;
+
+            _bytes[0, y, x] = color.R;
+            _bytes[1, y, x] = color.G;
+            _bytes[2, y, x] = color.B;
+        }
+
+        private Color _getPixelInBytes(int x, int y)
+        {
+            return Color.FromArgb(
+                _bytes[0, y, x],
+                _bytes[1, y, x],
+                _bytes[2, y, x]);
+        }
+
+        private delegate void DrawMethod(GTDrawerSlow self, Primitive2D primitive);
+
+        private delegate void AlgorithmDrawingLine(GTDrawerSlow self, Line2D line);
     }
-    delegate void DrawMethod(GTDrawerSlow self, Primitive2D primitive);
-    delegate void AlgorithmDrawingLine(GTDrawerSlow self, Line2D line);
 }
