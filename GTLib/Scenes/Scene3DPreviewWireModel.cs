549b2035650ce304ca8ee7786836ed26c0640611
Xilosof <pro100temi444@gmail.com>
2020-05-28 23:19:53 +0300

    Scene to demos one wire model

---
+++ GTLib/Scenes/Scene3DPreviewWireModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using GTLib.Cameras;
+using GTLib.Elements;
+using GTLib.Interfaces;
+using GTLib.Primitives;
+
+namespace GTLib.Scenes
+{
+    public class Scene3DPreviewWireModel : Scene3D
+    {
+        public WireModel Model { get; set; }
+        public Scene3DPreviewWireModel(WireModel model,Camera camera):base(camera)
+        {
+            Model = model;
+        }
+        public Scene3DPreviewWireModel(WireModel model):base() { }
+        public Scene3DPreviewWireModel() : this(new WireModel()) { }
+
+
+        public override List<Primitive> GetElements()
+        {
+            throw new NotImplementedException();
+        }
+        public override List<Primitive3D> Get3DElements()
+        {
+            return new List<Primitive3D>() { Model };
+        }
+    }
+}
