29c2f53506c12340204f9b9ea57afdb5272e022e
Xilosof <pro100temi444@gmail.com>
2020-05-28 23:14:18 +0300

    Added bool flag isNormalized for test. Its more quickly than check everytime when add any element.

--- GTLib/Elements/WireModel.cs
+++ GTLib/Elements/WireModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using System.Runtime.Intrinsics;
 using System.Text;
@@ -9,20 +10,63 @@ namespace GTLib.Elements
 {
     public class WireModel : Element3D
     {
-        public new List<Line3D> Primitives { get; protected set; } = new List<Line3D>();
-        public new List<Dot3D> DeclarativePrimitives { get; protected set; } = new List<Dot3D>();
+        private Boolean isNormalizedPrimitives;
+        private Boolean isNormalizedDeclarativePrimitives;
+
+        private new List<Line3D> _primitives = new List<Line3D>();
+        public new List<Line3D> Primitives
+        {
+            get
+            {
+                if(!isNormalizedPrimitives)
+                    this.NormalizePrimitives();
+                return _primitives;
+            }
+            
+        } 
+        private new List<Dot3D> _declarativePrimitives = new List<Dot3D>();
+
+        public new List<Dot3D> DeclarativePrimitives
+        {
+            get
+            {
+                if (!isNormalizedDeclarativePrimitives)
+                    this.NormalizeDeclarativePrimitives();
+                return _declarativePrimitives;
+
+            }
+        }
 
         public void AddWire(Line3D wire)
         {
             //if (Primitives.Contains(wire)) return;
 
-            Primitives.Add(wire);
+            _primitives.Add(wire);
 
             //if (!DeclarativePrimitives.Contains(wire.start))
-                DeclarativePrimitives.Add(wire.start);
+            _declarativePrimitives.Add(wire.start);
 
             //if (!DeclarativePrimitives.Contains(wire.finish))
-                DeclarativePrimitives.Add(wire.finish);
+            _declarativePrimitives.Add(wire.finish);
+
+            isNormalizedPrimitives = false;
+            isNormalizedDeclarativePrimitives = false;
+        }
+
+        public void Normalize()
+        {
+            NormalizePrimitives();
+            NormalizeDeclarativePrimitives();
+        }
+        public void NormalizePrimitives()
+        {
+            _primitives = new List<Line3D>(_primitives.Distinct());
+            isNormalizedPrimitives = true;
+        }
+        public void NormalizeDeclarativePrimitives()
+        {
+            _declarativePrimitives = new List<Dot3D>(_declarativePrimitives.Distinct());
+            isNormalizedDeclarativePrimitives = true;
         }
 
         public WireModel(Dot3D position) : base(position)
