7a5610e6e16caf2a4ec538fc007d2c4ae1e74065
Xilosof <pro100temi444@gmail.com>
2020-05-28 23:16:24 +0300

    abstract class renderer. He has scene3d

---
+++ GTLib/Renderers/Renderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using GTLib.Interfaces;
+using GTLib.Scenes;
+
+namespace GTLib.Renderers
+{
+    public abstract class Renderer:IGTRender
+    {
+        public Scene3D Scene3D { get; set; }
+
+        public virtual void Render()
+        {
+        }
+        public virtual uint RenderWithMetric()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Render();
+
+            stopWatch.Stop();
+            //return (UInt32)stopWatch.ElapsedTicks;
+            var seconds = stopWatch.ElapsedTicks / (double)Stopwatch.Frequency;
+            var nanoseconds = seconds * 1000000000;
+            return (uint)nanoseconds;
+        }
+
+    }
+}
