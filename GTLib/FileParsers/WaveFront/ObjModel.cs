59598df6221bb8ab2329623e67119258009d98a6
Xilosof <pro100temi444@gmail.com>
2020-05-28 22:48:56 +0300

    Classes who presents .obj file

---
+++ GTLib/FileParsers/WaveFront/ObjModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using GTLib.Primitives;
+
+namespace GTLib
+{
+    public class ObjModel
+    {
+        public List<Dot3D> vertices = new List<Dot3D>();
+        public List<Vector2> verticesTexture = new List<Vector2>();
+        public List<Vector3> verticesNormal = new List<Vector3>();
+        public List<List<Vector3>> faces = new List<List<Vector3>>();
+    }
+}
