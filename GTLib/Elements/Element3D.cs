7250d927ee599692919a111f4ceeec769307feb0
Xilosof <pro100temi444@gmail.com>
2020-05-28 22:42:46 +0300

    Element3D now have Dot3D position and Dot3D as Dot3D focus point.

--- GTLib/Elements/Element3D.cs
+++ GTLib/Elements/Element3D.cs
@@ -7,8 +7,18 @@ namespace GTLib.Elements
 {
     public abstract class Element3D : Primitive3D
     {
-        public double X { set; get; } = 0;
-        public double Y { set; get; } = 0;
-        public double Z { set; get; } = 0;
+        public List<Primitive3D> Primitives { get; protected set; }
+        public List<Primitive3D> DeclarativePrimitives { get; protected set; }
+
+        public Dot3D Position { get; set; }
+        public Dot3D Curs { get; set; }
+        public Element3D(Dot3D position)
+        {
+            Position = position;
+        }
+        public Element3D()
+        {
+            Position = new Dot3D(0,0,0);
+        }
     }
 }
