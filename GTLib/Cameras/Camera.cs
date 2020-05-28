4b0fabcb38ad4f2f6bfe0344e23f92d4b0095b91
Xilosof <pro100temi444@gmail.com>
2020-05-28 22:35:44 +0300

    Created Camera, which defined pos and vector spectrator in scene3D

---
+++ GTLib/Cameras/Camera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTLib.Primitives;
+
+namespace GTLib.Cameras
+{
+    public class Camera
+    {
+        public Dot3D Position;
+        public Dot3D CursNormal;
+        public Dot3D Top;
+    }
+}
