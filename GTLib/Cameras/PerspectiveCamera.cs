40ec16dbf98a4d31c2c49b8bf74ce01b49df30bc
Xilosof <pro100temi444@gmail.com>
2020-05-31 18:19:09 +0300

    Adding Perspective Camera

---
+++ GTLib/Cameras/PerspectiveCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace GTLib.Cameras
+{
+    public class PerspectiveCamera : Camera
+    {
+        public PerspectiveCamera(Vector3 eye, Vector3 focus, Vector3 up) : base(eye, focus, up)
+        {
+        }
+    }
+}
