4513a924e63ce76939de398ed2f019d4c046859d
Xilosof <pro100temi444@gmail.com>
2020-05-31 18:24:25 +0300

    Create Primitive FilledTriangle with him color

---
+++ GTLib/Primitives/FilledTriangle2D.cs
@@ -0,0 +1,25 @@
+﻿using GTLib.Elements;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GTLib.Primitives
+{
+    public class FilledTriangle2D : Primitive2D
+    {
+        public Color Color { get; set; }
+        public Dot2D A { get; set; }
+        public Dot2D B { get; set; }
+        public Dot2D C { get; set; }
+
+
+        public FilledTriangle2D(Dot2D a, Dot2D b, Dot2D c, Color color)
+        {
+            A = a;
+            B = b;
+            C = c;
+            Color = color;
+        }
+    }
+}
