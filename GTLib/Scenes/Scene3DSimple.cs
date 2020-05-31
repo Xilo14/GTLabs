b30fd541505e13c89298d91bc930f0cacf7c7ec9
Юрий Дьяченко <yurokke.yury@gmail.com>
2020-05-31 20:56:41 +0300

    2

---
+++ GTLib/Scenes/Scene3DSimple.cs
@@ -0,0 +1,23 @@
+﻿using GTLib.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Scenes
+{
+    class Scene3DSimple : Scene3D
+    {
+
+
+
+        public override List<Primitive3D> Get3DElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Primitive> GetElements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
