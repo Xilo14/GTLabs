56cca4003ba322b3f1e7f687e0d2d6973f77b550
Юрий Дьяченко <yurokke.yury@gmail.com>
2020-06-02 21:45:09 +0300

    created abstract class Light.cs and class DirectLight.cs

---
+++ GTLib/Light/DirectLight.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTLib.Light
+{
+    class DirectLight : Light
+    {
+        private Vector3 _lightVector;
+
+        public Vector3 LightVector { get => _lightVector; set => _lightVector = value; }
+    }
+}
