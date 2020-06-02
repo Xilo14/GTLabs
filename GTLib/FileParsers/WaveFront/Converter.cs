ce6a233ecff2e1c919bf9e9c1d2330604449e923
Xilosof <pro100temi444@gmail.com>
2020-06-02 22:19:56 +0300

    Added field DrawNormal, special for visualization normal vectors in vertices

--- GTLib/FileParsers/WaveFront/Converter.cs
+++ GTLib/FileParsers/WaveFront/Converter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using GTLib.Elements;
 using GTLib.Primitives;
@@ -9,6 +10,7 @@ namespace GTLib.FileParsers.WaveFront
 {
     public static class Converter
     {
+        public static bool DrawNormal = false;
         public static WireModel ObjModelToWireModel(ObjModel objModel)
         {
             var wireModel = new WireModel();
@@ -41,10 +43,67 @@ namespace GTLib.FileParsers.WaveFront
                         objModel.vertices[(int)(face[0].X - 1)].Y,
                         objModel.vertices[(int)(face[0].X - 1)].Z)));
 
+
+                if (DrawNormal)
+                    wireModel.AddWire(GetNormalLine(face,objModel));
+                if (DrawNormal)
+                    wireModel.AddWire(GetNormalLine1(face, objModel));
+                if (DrawNormal)
+                    wireModel.AddWire(GetNormalLine2(face, objModel));
             }
 
             return wireModel;
         }
+
+        private static Line3D GetNormalLine(List<Vector3> face,ObjModel model)
+        {
+            var n = model.verticesNormal[(int)face[0].Z - 1];
+            n =  0.1f * n;
+            var v = model.vertices[(int)face[0].X - 1];
+            n.X +=(float) v.X;
+            n.Y += (float)v.Y;
+            n.Z += (float)v.Z;
+            var line3d = new Line3D(
+                new Dot3D(n.X,n.Y,n.Z), 
+                new Dot3D(v.X,v.Y,v.Z));
+
+
+
+            return line3d;
+        }
+        private static Line3D GetNormalLine1(List<Vector3> face, ObjModel model)
+        {
+            var n = model.verticesNormal[(int)face[1].Z - 1];
+            n = 0.1f * n;
+            var v = model.vertices[(int)face[1].X - 1];
+            n.X += (float)v.X;
+            n.Y += (float)v.Y;
+            n.Z += (float)v.Z;
+            var line3d = new Line3D(
+                new Dot3D(n.X, n.Y, n.Z),
+                new Dot3D(v.X, v.Y, v.Z));
+
+
+
+            return line3d;
+        }
+        private static Line3D GetNormalLine2(List<Vector3> face, ObjModel model)
+        {
+            var n = model.verticesNormal[(int)face[2].Z - 1];
+            n = 0.1f * n;
+            var v = model.vertices[(int)face[2].X - 1];
+            n.X += (float)v.X;
+            n.Y += (float)v.Y;
+            n.Z += (float)v.Z;
+            var line3d = new Line3D(
+                new Dot3D(n.X, n.Y, n.Z),
+                new Dot3D(v.X, v.Y, v.Z));
+
+
+
+            return line3d;
+        }
+
         public static FilledTriangleModel ObjFilledToTriangleModel(ObjModel objModel)
         {
             var Model = new FilledTriangleModel();
